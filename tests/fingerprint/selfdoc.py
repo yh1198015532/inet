d8dbac92639b39223be7979f1b035e1c504b8a20
Rudolf Hornig <rudi@omnetpp.org>
2020-04-24 18:46:34 +0200

    added converter to create a proper structured JSON file

---
+++ tests/fingerprint/selfdoc.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import json
+
+with open('selfdoc.txt', 'r') as f:
+    content = "[" + f.read() + '{} ]'  # make an array from the individual dictionary items in the file and add an empty entry at the end to avoid issues with th trailing comma
+    database = json.loads(content)
+
+nedtypes = dict()
+
+for entry in database:
+    if entry : # skip empty entries (so you can add an empty entry at the end of the file)
+        nedtypes[entry["module"]] = actions = nedtypes.get(entry["module"], dict())
+        actions[entry["action"]] = details = actions.get(entry["action"], list())
+        details.append(entry["details"])
+
+with open('selfdoc.json', 'w') as f:
+    f.write(json.dumps(nedtypes, indent=True))
+    f.close
